b028190b01e4562ab21e9af55d0e18733999cfc2
jzazooro <91785177+jzazooro@users.noreply.github.com>
2021-11-12 09:49:43 +0100

    subida

--- adivinaelnumero.py
+++ adivinaelnumero.py
@@ -3,15 +3,15 @@ numero=random.randint(0,99)
 intento=int(input("Por favor introduzca un numero entre 0 y 99: "))
 intentosrealizados=0
 while intento != numero:
-    if intento<0 or intento>99:
+    while intento<0 or intento>99:
         print("El numero escogido no es valido")
         intentosrealizados=intentosrealizados+1
         intento=int(input("Por favor introduzca un nuevo numero: "))
-    if numero>intento:
+    while numero>intento:
         print("El numero escogido es demasiado pequeño")
         intentosrealizados=intentosrealizados+1
         intento=int(input("Por favor introduzca un nuevo numero: "))
-    if numero<intento:
+    while numero<intento:
         print("El numero escogido es demasiado grande")
         intentosrealizados=intentosrealizados+1
         intento=int(input("Por favor introduzca un numero nuevo: "))
